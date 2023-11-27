b91988c4ce68bfff2d91685b07a2199f83f15460
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-11-27 16:03:02 -0400

    SDAFASDFASDF

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ Vertices = [
     'K', 'L', 'M', 'N', 'P'
 ]
 
+Aristas = {
+    'A-B': 8, 'A-E': 4, 'A-D': 5, 
+    'B-E': 12, 'B-F': 4, 'B-C': 3, 
+    'C-F': 9, 'C-G': 11, 
+    'D-E': 9, 'D-H': 6
+}
+
 def recorrido_busqueda_anchura(): 
     pass
 
@@ -17,7 +24,10 @@ def algoritmo_prim():
     pass
 
 def main(): 
-    pass
+    recorrido_busqueda_anchura()
+    recorrido_busqueda_profundidad()
+    algoritmo_kruskal()
+    algoritmo_prim()
 
 if __name__ == '__main__': 
     main()
