bfb8e01555b305db95309c0f4f3914723bc359b6
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-11-27 12:10:41 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+Vertices = [
+    'A', 'B', 'C', 'D', 'E', 
+    'F', 'G', 'H', 'I', 'J', 
+    'K', 'L', 'M', 'N', 'P'
+]
+
+def recorrido_busqueda_anchura(): 
+    pass
+
+def recorrido_busqueda_profundidad():
+    pass
+
+def algoritmo_kruskal(): 
+    pass
+
+def algoritmo_prim(): 
+    pass
+
+def main(): 
+    pass
+
+if __name__ == '__main__': 
+    main()
