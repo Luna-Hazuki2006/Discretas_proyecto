3ebf77d447845252a860799553e1daf8d1f4b5ad
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-11-30 16:48:02 -0400

    aaaaaaaaaaaaaaaaaaaaaaa

--- app.py
+++ app.py
@@ -43,33 +43,33 @@ def algoritmo_kruskal():
     Nuevos_Vertices = []
     listaNumerica = []
     todo = max(sorted(list(Aristas.values())))
-    for i in range(1, todo): 
+    for i in range(1, todo + 1): 
         lista = list(filter(lambda x: Aristas[x] == i, Aristas.keys()))
         listaNumerica.append(lista)
     for esto in listaNumerica: 
         for este in esto: 
             primero = este[0]
             segundo = este[2]
-            if (primero not in Nuevos_Vertices and 
-                segundo not in Nuevos_Vertices): 
+            if (not Nuevos_Vertices.__contains__(primero) and 
+                not Nuevos_Vertices.__contains__(segundo)): 
                 Nuevos_Vertices.append(primero)
                 Nuevos_Vertices.append(segundo)
                 Nuevas_Aristas.append(este)
-            elif (primero not in Nuevos_Vertices and 
-                  segundo in Nuevos_Vertices): 
+            elif (not Nuevos_Vertices.__contains__(primero) and 
+                  Nuevos_Vertices.__contains__(segundo)): 
                 Nuevos_Vertices.append(primero)
                 Nuevas_Aristas.append(este)
-            elif (primero in Nuevos_Vertices and 
-                  segundo not in Nuevos_Vertices): 
+            elif (Nuevos_Vertices.__contains__(primero) and 
+                  not Nuevos_Vertices.__contains__(segundo)): 
                 Nuevos_Vertices.append(segundo)
                 Nuevas_Aristas.append(este)
             else: 
                 Nuevas_Aristas.append(este)
-            # print(Nuevas_Aristas)
+            if len(Nuevas_Aristas) is (len(Vertices) - 1): break
         if len(Nuevas_Aristas) is (len(Vertices) - 1): break
 
     resultado = sum((Aristas[x]) for x in Nuevas_Aristas)
-    
+    print(listaNumerica)
     print("*********************************")
     print('Algoritmo de Kruskal')
     print('Aristas: ')
@@ -80,23 +80,73 @@ def algoritmo_kruskal():
 def algoritmo_prim(): 
     Nuevas_Aristas = []
     Nuevos_Vertices = []
-    Aristas_Restantes = list(Aristas.keys())
+    Aristas_Restantes = []
+    maximo = len(Vertices) - 1
     actual = Vertices[0]
+    todo = max(sorted(list(Aristas.values())))
+    for i in range(1, todo + 1): 
+        lista = list(filter(lambda x: Aristas[x] == i, Aristas.keys()))
+        for esto in lista: Aristas_Restantes.append(esto)
+    print(Aristas_Restantes)
 
-    while len(Nuevos_Vertices) <= 15: 
-        busqueda = list(filter(lambda x: actual in x, Aristas_Restantes))
-        print(busqueda)
-        break
+    while len(Nuevas_Aristas) < maximo: 
+        if not Nuevos_Vertices.__contains__(actual): 
+            Nuevos_Vertices.append(actual)
+        lista = []
+        for esto in Nuevos_Vertices: 
+            prueba = list(filter(lambda x: (x.__contains__(esto)), 
+                                            Aristas.keys()))
+            for este in prueba: lista.append(este)
+        print(Aristas_Restantes)
+        print(lista)
+        lista = sorted(lista, key=lambda x: Aristas[x])
+        menor = lista[0]
+        print(menor)
+        Nuevas_Aristas.append(menor)
+        Aristas_Restantes = list(filter(lambda x: x != menor, Aristas_Restantes))
+        print(Nuevas_Aristas)
+        print(Nuevos_Vertices)
+        primero = menor[0]
+        segundo = menor[2]
+        if primero == menor: actual = segundo
+        elif segundo == menor: actual = primero
+
+    # for esto in Vertices: 
+    #     Nuevos_Vertices.append(esto)
+    #     lista = list(filter(lambda x: esto in x, Aristas.keys()))
+    #     lista = sorted(lista, key=lambda x: Aristas[x])
+    #     menor = lista[0]
+    #     if Aristas_Restantes.__contains__(menor): 
+    #         Nuevas_Aristas.append(menor)
+    #         Aristas_Restantes.remove(menor)
+        # if not Nuevos_Vertices.__contains__(esto): 
+        #     Nuevos_Vertices.append(esto)
+        # lista = list(filter(lambda x: esto in x, Aristas))
+        # lista = sorted(lista, key=lambda x: Aristas[x])
+        # menor = lista[0]
+        # if (not Nuevos_Vertices.__contains__(menor) and 
+        #     not Nuevas_Aristas.__contains__(esto)): 
+        #     Nuevas_Aristas.append(menor)
+            # Aristas_Restantes.remove(menor)
+            # lista = list(filter(lambda x: (Aristas[x] == Aristas[menor]), lista))
+            # if len(lista) == 1: 
+            #     Nuevas_Aristas.append(menor)
+            #     Aristas_Restantes.remove(menor)
+            # elif len(list(filter(lambda x: Nuevas_Aristas.__contains__(x), lista))) == 0: 
+            #     Nuevas_Aristas.append(menor)
+            #     Aristas_Restantes.remove(menor)
+    
+    resultado = sum((Aristas[x]) for x in Nuevas_Aristas)
 
     print("*********************************")
     print('Algoritmo de Prim')
-    print('Aristas: ')
-    print(Nuevas_Aristas)
     print('Aristas restantes: ')
     print(Aristas_Restantes)
+    print('Aristas: ')
+    print(Nuevas_Aristas)
     print('Vertices: ')
     print(Nuevos_Vertices)
-    print(f"La ponderación mínima es de: 0")
+    print(f"La ponderación mínima es de: {resultado}")
     print('*********************************')
 
 def main(): 
