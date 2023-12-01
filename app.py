3c89a52cd9cfd6247347e410a0d0f064c3f3e936
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-12-01 10:02:57 -0400

    mas cosas

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from pprint import pprint
+import matplotlib.pyplot as graficador
+import networkx as grafo_Oficial
 
 Vertices = [
     'A', 'B', 'C', 'D', 'E', 
@@ -31,12 +33,23 @@ def recorrido_busqueda_anchura():
     Nuevos_Vertices = []
     data = []
     actual = Vertices[0]
-    final = list(filter(lambda x: actual in x, Aristas.keys()))
-    
+    while len(Vertices) - 1 == len(Nuevas_Aristas): 
+        cercanos = list(filter(lambda x: x.__contains__(actual), Aristas.keys()))
+        for esto in cercanos: 
+            print(esto)
+    resultado = 0
+    print("*********************************")
+    print('Recorrido de busqueda anchura')
+    print('Aristas: ')
+    print(Nuevas_Aristas)
+    print(f"La ponderación mínima es de: {resultado}")
+    print('*********************************')
+    return Nuevas_Aristas
 
 def recorrido_busqueda_profundidad():
     Nuevas_Aristas = []
     Nuevos_Vertices = []
+    return Nuevas_Aristas
 
 def algoritmo_kruskal(): 
     Nuevas_Aristas = []
@@ -76,6 +89,7 @@ def algoritmo_kruskal():
     print(Nuevas_Aristas)
     print(f"La ponderación mínima es de: {resultado}")
     print('*********************************')
+    return Nuevas_Aristas
 
 def algoritmo_prim(): 
     Nuevas_Aristas = []
@@ -87,34 +101,42 @@ def algoritmo_prim():
     for i in range(1, todo + 1): 
         lista = list(filter(lambda x: Aristas[x] == i, Aristas.keys()))
         for esto in lista: Aristas_Restantes.append(esto)
-    print(Aristas_Restantes)
+    # print(Aristas_Restantes)
 
-    while len(Nuevas_Aristas) < maximo: 
+    while (len(Nuevas_Aristas) < maximo) and (len(Nuevos_Vertices) != len(Vertices)): 
+        print(actual)
         if not Nuevos_Vertices.__contains__(actual): 
             Nuevos_Vertices.append(actual)
         lista = []
         for esto in Nuevos_Vertices: 
-            prueba = list(filter(lambda x: (x.__contains__(esto) and 
-                                            not Nuevas_Aristas.__contains__(esto)), 
+            prueba = list(filter(lambda x: (esto in x), 
                                             Aristas_Restantes))
-            for este in prueba: 
-                lista.append(este)
-                lista = list(set(lista))
+            print(prueba)
+            for este in prueba: lista.append(este)
+        
+        lista = list(set(lista))
+       
         lista = sorted(lista, key=lambda x: Aristas[x])
+        # print(lista)
         menor = lista[0]
+        # if actual == 'C' or actual == 'B': 
         Nuevas_Aristas.append(menor)
         Aristas_Restantes = list(filter(lambda x: x != menor, Aristas_Restantes))
         primero = menor[0]
         segundo = menor[2]
         verdad = [primero, segundo]
-        if primero != menor: actual = segundo
-        elif segundo != menor: actual = primero
+        for este in verdad: Nuevos_Vertices.append(este)
+        Nuevos_Vertices = list(set(Nuevos_Vertices))
+        if primero != menor: 
+            actual = segundo
+        elif segundo != menor: 
+            actual = primero
 
     
-    retorno = Nuevas_Aristas.pop()
-    Nuevas_Aristas.append(Aristas_Restantes[0])
-    Aristas_Restantes.remove(Aristas_Restantes[0])
-    Aristas_Restantes.append(retorno)
+    # retorno = Nuevas_Aristas.pop()
+    # Nuevas_Aristas.append(Aristas_Restantes[0])
+    # Aristas_Restantes.remove(Aristas_Restantes[0])
+    # Aristas_Restantes.append(retorno)
 
     # for esto in Vertices: 
     #     Nuevos_Vertices.append(esto)
@@ -153,12 +175,26 @@ def algoritmo_prim():
     print(Nuevos_Vertices)
     print(f"La ponderación mínima es de: {resultado}")
     print('*********************************')
+    return Nuevas_Aristas
 
 def main(): 
-    recorrido_busqueda_anchura()
+    lista = recorrido_busqueda_anchura()
     # recorrido_busqueda_profundidad()
-    algoritmo_kruskal()
+    lista = algoritmo_kruskal()
     # algoritmo_prim()
+    print("Cierra la ventana para continuar...")
+    GRAFO = grafo_Oficial.Graph()
+    print(Nuevas_Aristas)
+    lista = list(map(lambda x: (x[0], x[2], Aristas[x]), lista))
+    print('litas')
+    print(lista)
+    GRAFO.add_weighted_edges_from(lista)
+
+    pos=grafo_Oficial.spring_layout(GRAFO, seed=4)
+    grafo_Oficial.draw_networkx(GRAFO,pos)
+    labels = grafo_Oficial.get_edge_attributes(GRAFO,'weight')
+    grafo_Oficial.draw_networkx_edge_labels(GRAFO,pos,edge_labels=labels)
+    # graficador.show()
 
 if __name__ == '__main__': 
     main()
